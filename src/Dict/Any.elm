6201acfa072eece0d3d690138650cd2611e0ffd4
Marek Fajkus <marek.faj@gmail.com>
2020-03-02 13:03:42 +0100

    improve docs comment

--- src/Dict/Any.elm
+++ src/Dict/Any.elm
@@ -117,7 +117,10 @@ type AnyDict comparable k v
         }
 
 
-{-| Check equality on two `AnyDict`s
+{-| Check equality of two `AnyDict`s
+
+    * returns `True` if AnyDicts are equal
+    * returns `False` if AnyDicts are not equal
 -}
 equal : AnyDict comparable k v -> AnyDict comparable k v -> Bool
 equal (AnyDict r1) (AnyDict r2) =
